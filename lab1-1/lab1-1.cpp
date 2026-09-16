2fc5e7b3a7004a1a016adb70ac1188f12c18d484
AlexeyProh <alexproh132@mail.ru>
2026-09-16 18:37:46 +0300

    Update matrix

--- lab1-1/lab1-1.cpp
+++ lab1-1/lab1-1.cpp
@@ -1,14 +1,17 @@
 #include "lab1-1.h"
 #include <windows.h>
 
-int input(){
+int input()
+{
     int temp;
-    while (true) {
+    while (true)
+    {
         std::cin >> temp;
-        
-        if (std::cin.fail() || temp < 0) {
-            std::cin.clear(); 
-            std::cin.ignore(10000, '\n');  
+
+        if (std::cin.fail() || temp < 0)
+        {
+            std::cin.clear();
+            std::cin.ignore(10000, '\n');
             std::cout << "Введите целое положительное число: ";
             continue;
         }
@@ -17,40 +20,51 @@ int input(){
     return temp;
 }
 
-int main(){
+int main()
+{
     SetConsoleCP(CP_UTF8);
     SetConsoleOutputCP(CP_UTF8);
+
     int rows, cols;
+
     std::cout << "Введите размер первой матрицы (строки x столбцы): ";
     rows = input();
     cols = input();
-    
     Matrix M1(rows, cols);
     M1.inputMatrix();
 
     std::cout << "Введите размер второй матрицы (строки x столбцы): ";
     rows = input();
     cols = input();
-
     Matrix M2(rows, cols);
     M2.inputMatrix();
 
+    std::cout << "Матрица M1:\n";
+    M1.printMatrix();
+    std::cout << "Матрица M2:\n";
+    M2.printMatrix();
+
+    std::cout << "\nСложение (M3 = M1 + M2)\n";
     Matrix M3 = M1 + M2;
     M3.printMatrix();
 
+    std::cout << "\nУмножение (M4 = M1 * M2)\n";
     Matrix M4 = M1 * M2;
     M4.printMatrix();
 
+    std::cout << "\nКопирование (M5 = M1)\n";
+    Matrix M5 = M1;
+    M5.printMatrix();
 
-    
+    std::cout << "\nПроверка += (M5 += M2)\n";
+    M5 += M2;
+    M5.printMatrix();
 
+    std::cout << "\nПроверка *= (M5 *= M2)\n";
+    M5 *= M2;
+    M5.printMatrix();
 
-
-
-
-
-
-
-
-
+    std::cout << "\nПроверка присваивания (M5 = M2)\n";
+    M5 = M2;
+    M5.printMatrix();
 }
--- lab1-1/lab1-1.h
+++ lab1-1/lab1-1.h
@@ -1,77 +1,56 @@
-#pragma once 
+#pragma once
 #include <iostream>
 
-class Matrix {
+class Matrix
+{
 
     int rows{0}, cols{0};
-    int **matrix{nullptr};
+    int *matrix{nullptr};
 
 public:
-
     Matrix() = default;
 
     Matrix(int r, int c) : rows(r), cols(c)
     {
-        if(rows != 0 && cols != 0){
-            matrix = new int*[rows];
-            for (size_t i = 0; i < rows; i++)
-            {
-                matrix[i] = new int[cols];
-            }
-        }
+        if (rows != 0 && cols != 0)
+            matrix = new int[rows * cols]{};
         else
             matrix = nullptr;
     }
 
-    Matrix(const Matrix& other) : Matrix(other.rows, other.cols) 
+    Matrix(const Matrix &other) : Matrix(other.rows, other.cols)
     {
-        for (size_t i = 0; i < rows; i++)
+        int size = rows * cols;
+        for (size_t i = 0; i < size; i++)
         {
-            for (size_t j = 0; j < cols; j++)
-            {
-                matrix[i][j] = other.matrix[i][j];
-            }
+            matrix[i] = other.matrix[i];
         }
     }
 
     ~Matrix()
     {
-        if (matrix) {
-            for (size_t i = 0; i < rows; i++)
-            {
-                delete[] matrix[i];
-            }
-            delete[] matrix;
-        }
+        delete[] matrix;
     }
 
+    Matrix &operator=(const Matrix &other)
+    {
+        if (this != &other)
+        {
 
-    Matrix& operator=(const Matrix& other) {
-        if (this != &other) {
-
-            if(matrix){
-            for (size_t i = 0; i < rows; i++)
-                {
-                    delete[] matrix[i];
-                }
             delete[] matrix;
-            }
 
             rows = other.rows;
             cols = other.cols;
 
-            if(rows != 0 && cols != 0){
-            matrix = new int*[rows];
-            for (size_t i = 0; i < rows; i++)
+            if (rows != 0 && cols != 0)
             {
-                matrix[i] = new int[cols];
-            }
+                matrix = new int[rows * cols];
 
-            for (size_t i = 0; i < rows; ++i) {
-                for (size_t j = 0; j < cols; ++j) {
-                    matrix[i][j] = other.matrix[i][j];
+                int size = rows * cols;
+                for (size_t i = 0; i < size; ++i)
+                {
+                    matrix[i] = other.matrix[i];
                 }
-            }
             }
             else
                 matrix = nullptr;
@@ -79,44 +58,46 @@ public:
         return *this;
     }
 
-    Matrix operator+(const Matrix& other) const
+    Matrix operator+(const Matrix &other) const
     {
-        if(rows != other.rows || cols != other.cols)
+        if (rows != other.rows || cols != other.cols)
         {
             std::cout << "нельзя сложить матрицы\n";
             return Matrix(0, 0);
         }
 
-        Matrix result(rows,cols);
-        
-        for (size_t i = 0; i < rows; i++)
+        Matrix result(rows, cols);
+
+        int size = rows * cols;
+        for (size_t i = 0; i < size; i++)
         {
-            for (size_t j = 0; j < cols; j++)
-            {
-              result.matrix[i][j] = matrix[i][j] + other.matrix[i][j];
-            }
+            result.matrix[i] = matrix[i] + other.matrix[i];
         }
         return result;
-    } 
+    }
 
-
-    Matrix& operator+=(const Matrix& other)
+    Matrix &operator+=(const Matrix &other)
     {
-        if(rows != other.rows || cols != other.cols)
+        if (rows != other.rows || cols != other.cols)
         {
             std::cout << "нельзя сложить матрицы\n";
             return *this;
         }
 
-        *this = *this + other;
+        int size = rows * cols;
+        for (int i = 0; i < size; ++i)
+        {
+            matrix[i] += other.matrix[i];
+        }
         return *this;
-    } 
+    }
 
-    Matrix operator*(const Matrix& other) const
+    Matrix operator*(const Matrix &other) const
     {
-        if (cols != other.rows) {
+        if (cols != other.rows)
+        {
             std::cout << "нельзя умножить матрицы\n";
-            return Matrix(0 , 0);
+            return Matrix(0, 0);
         }
 
         Matrix result(rows, other.cols);
@@ -124,22 +105,21 @@ public:
         {
             for (size_t j = 0; j < other.cols; j++)
             {
-               result.matrix[i][j] = 0;
-               for (size_t k = 0; k < cols; k++)
-               {
-                    result.matrix[i][j] += matrix[i][k] * other.matrix[k][j];
-               }
-
+                result.matrix[i * other.cols + j] = 0;
+                for (size_t k = 0; k < cols; k++)
+                {
+                    result.matrix[i * other.cols + j] += matrix[i * cols + k] * other.matrix[k * other.cols + j];
+                }
             }
-
         }
-        
+
         return result;
     }
 
-    Matrix& operator*=(const Matrix& other)
+    Matrix &operator*=(const Matrix &other)
     {
-        if (cols != other.rows) {
+        if (cols != other.rows)
+        {
             std::cout << "нельзя умножить матрицы\n";
             return *this;
         }
@@ -147,30 +127,34 @@ public:
         return *this;
     }
 
-    void printMatrix()
+    void printMatrix() const
     {
         for (size_t i = 0; i < rows; i++)
         {
             for (size_t j = 0; j < cols; j++)
             {
-               std::cout << " " << matrix[i][j] << " ";
+                std::cout << " " << matrix[i * cols + j] << " ";
             }
             std::cout << std::endl;
         }
     }
-    
-    void inputMatrix(){
-        if (rows == 0 || cols == 0) {
-        std::cout << "матрица пустая" << std::endl;
-        return;
-    }
-    
-    std::cout << "Введите элементы матрицы (" << rows << "x" << cols << "):" << std::endl;
-    for (size_t i = 0; i < rows; i++) {
-        std::cout << "Строка " << i + 1 << ": ";
-        for (size_t j = 0; j < cols; j++) {
-            std::cin >> matrix[i][j];
+
+    void inputMatrix()
+    {
+        if (rows == 0 || cols == 0)
+        {
+            std::cout << "матрица пустая" << std::endl;
+            return;
         }
-    }
+
+        std::cout << "Введите элементы матрицы (" << rows << "x" << cols << "):" << std::endl;
+        for (size_t i = 0; i < rows; i++)
+        {
+            std::cout << "Строка " << i + 1 << ": ";
+            for (size_t j = 0; j < cols; j++)
+            {
+                std::cin >> matrix[i * cols + j];
+            }
+        }
     }
 };
